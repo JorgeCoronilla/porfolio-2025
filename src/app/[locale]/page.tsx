6476ecbbbac54b12200cbc57c2f0e05bb88c9f76
JorgeCoronilla <jorge.coronilla.naranjo@gmail.com>
2025-03-10 11:26:26 +0100

    🐛 Fix 404 page TS error

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,15 +1,20 @@
 'use client';
 
-import { useTranslation } from "react-i18next";
-
+import { useTranslation } from 'react-i18next';
+import styles from './page.module.css';
+import LanguageSwitcher from '@/features/i18n/LanguageSwitcher';
 
 export default function Home() {
   const { t } = useTranslation();
 
   return (
-    <>
-    <h1>{t("Hero.title")}</h1>
-    <p>{t("Hero.description")}</p>
-    </>
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <LanguageSwitcher />
+
+        <h1>{t('Hero.title')}</h1>
+        <p>{t('Hero.description')}</p>
+      </main>
+    </div>
   );
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,23 +4,18 @@ import { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
 import i18nConfig from '@i18nConfig';
 
-// Import or recreate the Locale type
-type Locale = 'en' | 'es';
+
 
 export default function NotFound() {
   const [locale, setLocale] = useState<Locale>(i18nConfig.defaultLocale);
   const pathname = usePathname();
   
   useEffect(() => {
-    // Try to extract locale from URL
     const pathSegments = pathname.split('/');
-    // Type check to ensure pathSegments[1] is a valid Locale
     if (pathSegments.length > 1 && i18nConfig.locales.includes(pathSegments[1] as Locale)) {
       setLocale(pathSegments[1] as Locale);
     } else {
-      // If no locale in URL, check localStorage
       const savedLocale = localStorage.getItem('NEXT_LOCALE');
-      // Make sure savedLocale is a valid Locale before setting it
       if (savedLocale && i18nConfig.locales.includes(savedLocale as Locale)) {
         setLocale(savedLocale as Locale);
       } else {
