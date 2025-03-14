ba4410bbf09f49692d3adc0f3565508638514dcd
JorgeCoronilla <jorge.coronilla.naranjo@gmail.com>
2025-03-14 15:43:49 +0100

    Testing tooltip

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,22 +1,91 @@
 'use client';
 import Header from '@/features/ui/Header';
-import { useState } from 'react';
 import styles from './page.module.css';
+import { useState } from 'react';
+import LightModeIcon from '@/features/ui/icons/LightModeIcon';
+import DarkModeIcon from '@/features/ui/icons/DarkModeIcon';
+import { FaXTwitter } from "react-icons/fa6";
 
+import { FaReact, FaVuejs, FaNodeJs, FaJs, FaHtml5, FaSass, FaCss3 } from 'react-icons/fa';
+import { FaUniversalAccess, FaFont } from 'react-icons/fa6';
+import { RiFontSize } from 'react-icons/ri';
+import { BiFontSize } from 'react-icons/bi';
+import { RxFontSize } from 'react-icons/rx';
+import { SlMagnifierAdd } from 'react-icons/sl';
+import { SlMagnifierRemove } from 'react-icons/sl';
+import { BsFiletypeSql } from 'react-icons/bs';
+import { PiFileSqlFill } from 'react-icons/pi';
+import { RiNextjsFill } from 'react-icons/ri';
+import { BiSolidMicrophone } from 'react-icons/bi';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaSquareXTwitter } from 'react-icons/fa6';
+import { DiGithubAlt } from 'react-icons/di';
+import { BiLogoGmail } from 'react-icons/bi';
+import { FaLinkedinIn } from "react-icons/fa6";
+
+import { SiTypescript, SiCss3 } from 'react-icons/si';
 export default function Home() {
-  const [viewHeader, setViewHeader] = useState(false);
+  const [viewHero, setViewHero] = useState(false);
 
-  const changeView = () => {
-    setViewHeader(!viewHeader);
-  };
   return (
     <div className={styles.page}>
-      <Header viewHeader={viewHeader} />
+      <Header viewHero={viewHero} setViewHero={setViewHero} />
       <main className={styles.main}>
-        <section className={styles.section1}>
-          <button className={styles.startButton} onClick={changeView}>Start</button>
+        <section className={styles.section1} id="home">
+          {viewHero && (
+            <div className={styles.hero}>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <h1>Hero</h1>
+              <p>Hero</p>
+              <br></br>
+              <br></br>
+              <FaLinkedinIn />
+              <DiGithubAlt />
+              <FaXTwitter />
+              <BiLogoGmail />
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+
+              <LightModeIcon className={styles.icon} />
+              <DarkModeIcon className={styles.icon} />
+              <FaReact />
+              <SiTypescript />
+              <SiCss3 />
+              <RiNextjsFill />
+              <BiSolidMicrophone />
+       
+              <FaVuejs />
+              <FaNodeJs />
+              <FaJs />
+              <FaHtml5 />
+              <FaCss3 />
+              <FaSass />
+
+              <FaJs />
+              <FaUniversalAccess />
+              <RiFontSize />
+              <BiFontSize />
+              <RxFontSize />
+
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+            </div>
+          )}
         </section>
-        <section className={styles.section2}></section>
+        <section className={styles.section2} id="work"></section>
+        <section className={styles.section1} id="projects"></section>
+        <section className={styles.section2} id="about"></section>
+        <section className={styles.section1} id="contact"></section>
       </main>
     </div>
   );
--- src/features/ui/Header.tsx
+++ src/features/ui/Header.tsx
@@ -1,41 +1,137 @@
-'use client'
-import { useTranslation } from 'react-i18next';
-import LanguageSwitcher from '../i18n/LanguageSwitcher';
-import styles from './Header.module.css';
+'use client';
+
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import LanguageSwitcher from '../i18n/LanguageSwitcher';
+import { useTranslation } from 'react-i18next';
+
+import styles from './Header.module.css';
 
 interface HeaderProps {
-    viewHeader: boolean;
+  viewHero: boolean;
+  setViewHero: React.Dispatch<React.SetStateAction<boolean>>;
 }
-export default function Header({viewHeader}:HeaderProps) {
-  const { t } = useTranslation();
-  console.log(viewHeader);
+export default function Header({ viewHero, setViewHero }: HeaderProps) {
+  const { t, i18n } = useTranslation();
+  const currentLocale = i18n.language as Locale;
+  const [viewHeader, setViewHeader] = useState(false);
+  const [isOverNav, seIsOverNav] = useState(false);
+  const [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
+  const [scrollY, setScrollY] = useState(0);
+
+  const [isCustomCursorActive, setIsCustomCursorActive] = useState(false);
+
+  const handleMouseMove = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const { clientX, clientY } = event;
+    setCoordinates({ x: clientX - 40, y: clientY + scrollY });
+  };
+  const navHover = () => {
+    seIsOverNav(true);
+    console.log('Entra en el nav');
+  };
+  const navLeave = () => {
+    seIsOverNav(false);
+    console.log('Sale del nav');
+  };
+  const changeView = () => {
+    setIsCustomCursorActive(false);
+    setViewHeader(!viewHeader);
+    setViewHero(!viewHero);
+  };
+
+  const handleScroll = () => {
+    setScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log('scrollY', scrollY);
+    if (scrollY >= 0.2459 * window.innerHeight && !viewHeader) {
+      changeView();
+    }
+  }, [scrollY]);
+
   return (
-    <header className={`${viewHeader ? styles.asHeader : styles.asHero}`}>
-      <div className={styles.headerTitle}>
-        <p>{t('Hero.title')}</p>
-      </div>
-      <nav>
-        <Link href="#about" className={styles.link}>
-          <span>Work</span>
-       
-        </Link>
-        <Link href="#about" className={styles.link}>
-          <span>Projects</span>
-       
-        </Link>
-        <Link href="#about" className={styles.link}>
-          <span>About me</span>
-       
-        </Link>
-        <Link href="#about" className={styles.link}>
-          <span>Contact</span>
-       
-        </Link>
-      </nav>
-      <div className={styles.buttonsContainer}>
-        <LanguageSwitcher />
-      </div>
-    </header>
+    <>
+      <header className={`${viewHeader ? styles.asHeader : styles.asHero}`}>
+        <div className={styles.headerTitle}>
+          <p>{t('Hero.title')} </p>
+        </div>
+        <nav
+          onMouseMove={handleMouseMove}
+          onMouseLeave={() => setIsCustomCursorActive(false)}
+          onMouseEnter={() => setIsCustomCursorActive(true)}
+        >
+          <Link
+            href="#work"
+            className={`${styles.link}  ${styles.work} ${isOverNav ? styles.overNav : ''}`}
+            onMouseOver={() => navHover()}
+            onMouseLeave={() => navLeave()}
+          >
+            <span className={styles[`${currentLocale}`]}>{t('Header.work')}</span>
+          </Link>
+          <div className={`${isOverNav ? styles.tooltip: styles.tooltipHidden}`}>Tooltip1</div>
+
+          <Link
+            href="#projects"
+            className={`${styles.link}  ${styles.projects} ${isOverNav ? styles.overNav : ''}`}
+            onMouseOver={() => navHover()}
+            onMouseLeave={() => navLeave()}
+          >
+            <span className={styles[`${currentLocale}`]}>{t('Header.projects')}</span>
+          </Link>
+          {/* <div className={`${isOverNav ? styles.tooltip: styles.tooltipHidden}`}>Tooltip1</div> */}
+          <Link
+            href="#about"
+            className={`${styles.link}  ${styles.about} ${isOverNav ? styles.overNav : ''}`}
+            onMouseOver={() => navHover()}
+            onMouseLeave={() => navLeave()}
+          >
+            <span className={styles[`${currentLocale}`]}>{t('Header.about')}</span>
+          </Link>
+          {/* <div className={`${isOverNav ? styles.tooltip: styles.tooltipHidden}`}>Tooltip1</div> */}
+
+          <Link
+            href="#contact"
+            className={`${styles.link}  ${styles.contact} ${isOverNav ? styles.overNav : ''}`}
+            onMouseOver={() => navHover()}
+            onMouseLeave={() => navLeave()}
+          >
+            <span className={styles[`${currentLocale}`]}> {t('Header.contact')}</span>
+          </Link>
+          {/* <div className={`${isOverNav ? styles.tooltip: styles.tooltipHidden}`}>Tooltip1</div> */}
+
+        </nav>
+        <div className={styles.buttonsContainer}>
+          <LanguageSwitcher />
+        </div>
+      </header>
+
+      {!viewHeader && (
+        <button
+          className={styles.startButton}
+          onClick={changeView}
+          onMouseMove={handleMouseMove}
+          onMouseLeave={() => setIsCustomCursorActive(false)}
+          onMouseEnter={() => setIsCustomCursorActive(true)}
+        >
+          {/* {t('Hero.start')}{' '} */}
+          Frontend Developer
+        </button>
+      )}
+      {isCustomCursorActive && (
+        <div
+          className={styles['gg-arrow-top-right-o']}
+          style={{ top: `${coordinates.y}px`, left: `${coordinates.x}px` }}
+        ></div>
+      )}
+    </>
   );
 }
